2c63bdb2d7cb92aade1e8766491d8d37e5003735
KeremTurkyilmaz <kerem.turkyilmaz@outlook.it>
2020-10-20 22:44:58 +0200

    Basic draw function

--- src/components/Sketch/Controller/index.js
+++ src/components/Sketch/Controller/index.js
@@ -5,6 +5,8 @@ export default class Controller {
 		this.canvas = options.canvas
 		this.animate = options.animate
 	init() {
+		// Get canvas context
+		this.ctx = this.canvas.getContext('2d')
 		// Retrive container bounds
 		this.bounds = this.calculateBounds()
 		// Set canvas dimension
@@ -13,6 +15,16 @@ export default class Controller {
 		this.canvas.style.width = this.bounds.w + 'px'
 		this.canvas.style.height = this.bounds.h + 'px'
 	}
+
+	draw() {
+		if (this.animate) {
+			this.ctx.clearRect(0, 0, this.bounds.w, this.bounds.h)
+			this.ctx.strokeRect(this.bounds.center.x, this.bounds.center.y, 100, 100)
+			this.ctx.stroke()
+			requestAnimationFrame(() => this.draw())
+		}
+	}
+
 	calculateBounds() {
 		const container = this.canvas.parentNode
 		const containerSize = container.getBoundingClientRect()
