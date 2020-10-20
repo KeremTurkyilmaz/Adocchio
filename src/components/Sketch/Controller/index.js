101aa116e2eae5b898effcc0e8314c49bd238c2a
KeremTurkyilmaz <kerem.turkyilmaz@outlook.it>
2020-10-20 22:44:16 +0200

    calculate canvas parent bounds

--- src/components/Sketch/Controller/index.js
+++ src/components/Sketch/Controller/index.js
@@ -4,6 +4,25 @@ export default class Controller {
 		this.options = options
 		this.canvas = options.canvas
 		this.animate = options.animate
+	init() {
+		// Retrive container bounds
+		this.bounds = this.calculateBounds()
+	}
+	calculateBounds() {
+		const container = this.canvas.parentNode
+		const containerSize = container.getBoundingClientRect()
+		return {
+			x: containerSize.x,
+			y: containerSize.y,
+			w: containerSize.width,
+			h: containerSize.height,
+			center: {
+				x: containerSize.width / 2,
+				y: containerSize.height / 2
+			}
+		}
+	}
+
 	stop() {
 		this.animate = false
 		this.ctx.clearRect(0, 0, this.bounds.w, this.bounds.h)
