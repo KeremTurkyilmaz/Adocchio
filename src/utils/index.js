715c88df8401cf741060e87a15a33f2377fc8aef
KeremTurkyilmaz <kerem.turkyilmaz@outlook.it>
2020-10-22 00:08:40 +0200

    calculate-grid utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -17,3 +17,37 @@ export const normalize = (val, min, max) => (val - min) / (max - min)
 export const lerp = (start, end, amt) => {
 	return (1 - amt) * start + amt * end
 }
+
+export const calculateGrid = (options = {}) => {
+
+	// Check props
+	const modulo = options.modulo || 100
+	const bounds = options.bounds || { w: window.innerWidth, h: window.innerHeight }
+
+	// Calculate maximum rows and columns
+	const cols = Math.floor(bounds.width / modulo)
+	const rows = Math.floor(bounds.height / modulo)
+
+
+	// Calculate the size of all rows and columns
+	const dim = {
+		x: cols * modulo,
+		y: rows * modulo
+	}
+
+	// Calculate the remaining space
+	const offX = bounds.width - dim.x
+	const offY = bounds.height - dim.y
+
+	// Calcualte the padding between each column and row
+	const pad = {
+		x: offX / (cols + 1),
+		y: offY / (rows + 1)
+	}
+
+	return {
+		cols: cols,
+		rows: rows,
+		pad: pad
+	}
+}
