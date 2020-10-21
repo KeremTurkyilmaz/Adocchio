7f98b4f726c808a7ef29bed2581c49f526501561
KeremTurkyilmaz <kerem.turkyilmaz@outlook.it>
2020-10-21 23:58:07 +0200

    cleanup utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,25 +14,6 @@ export const clamp = (num, min, max) => {
 
 export const normalize = (val, min, max) => (val - min) / (max - min)
 
-export const setupWorkerListeners = obj => {
-	obj.listeners = []
-	obj.on = (channel, callback) => {
-		obj.listeners.push({ channel, callback })
-	}
-	obj.onmessage = m => {
-		obj.listeners.forEach(l => {
-			if (l.channel === m.data.channel) l.callback(m.data.value)
-		})
-	}
-}
-
-export const setupWorkerEmitters = obj => {
-	obj.emit = (channel, value) => {
-		obj.postMessage({ channel, value })
-	}
-	obj.send = obj.emit
-}
-
 export const lerp = (start, end, amt) => {
 	return (1 - amt) * start + amt * end
 }
