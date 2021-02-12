d5bfd37afd8fd6bd356907ecf8345c531f3d5582
Kerem Türkyilmaz <kerem.turkyilmaz@outlook.it>
2021-02-12 21:25:41 +0100

    first controls ui

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,43 @@
 <template>
   <div id="app">
-    <Sketch />
-    <Capture />
+    <Controls v-if="showControls && controller && capture" :controller="controller" :capture="capture" />
+    <Sketch ref="sketch" />
+    <Capture ref="capture" />
   </div>
 </template>
 
 <script>
+import Controls from '@/components/Controls.vue'
 import Capture from '@/components/detection/capture.vue'
 import Sketch from '@/components/sketch/'
 export default {
-  name: 'App',
-  components: {
-    Capture,
-    Sketch
-  }
+	name: 'App',
+	components: {
+		Controls,
+		Capture,
+		Sketch
+	},
+	data() {
+		return {
+			showControls: false,
+			controller: null,
+			capture: null
+		}
+	},
+	mounted() {
+		this.controller = this.$refs.sketch.controller
+		this.capture = this.$refs.capture
+		window.addEventListener('keypress', this.handleKeyPress)
+	},
+	destroyed() {
+		window.removeEventListener('keypress', this.handleKeyPress)
+	},
+	methods: {
+		handleKeyPress(e) {
+			if (e.key == 'd') this.showControls = !this.showControls
+		}
+	}
 }
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
---
+++ src/components/Controls.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="controls">
+    <p @click="toggleDebug">Enable Debug</p>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Controls',
+	props: {
+		controller: {
+			type: Object,
+			required: true
+		},
+		capture: {
+			type: Object,
+			required: true
+		}
+	},
+	methods: {
+		toggleDebug() {
+			this.capture.debug = !this.capture.debug
+			this.controller.updateOptions = {
+				key: 'debug',
+				value: !this.controller.options.debug
+			}
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.controls {
+	position: fixed;
+	top: 0;
+	left: 0;
+	padding: $padding;
+}
+</style>
